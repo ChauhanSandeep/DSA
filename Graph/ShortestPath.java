c91d12edcc45f5cfdf6b59f02c2fdaf197a50f63
SandeepChauhan <sschauhan@tekion.com>
2021-08-11 17:00:00 +0530

    Shotest path of zeroes

---
+++ Graph/ShortestPath.java
@@ -0,0 +1,62 @@
+package Graph;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Find min distance from top left to bottom right in binary matrix.
+ * Given that path can be created by 0s only. Traversal to all 8 adjacent block is allowed
+ * If there is no path possible return -1
+ */
+public class ShortestPath {
+
+  public int[][] directions = {{-1, -1},{-1, 0},{-1, 1},{0, -1},{0, 1},{1, -1},{1, 0},{1, 1},};
+  public static void main(String[] args) {
+    int[][] grid = {
+        {0,0,0},
+        {1,0,0},
+        {1,1,0}
+    };
+    int shortestPath = new ShortestPath().shortestPathBinaryMatrix(grid);
+    System.out.println(shortestPath);
+  }
+
+  public int shortestPathBinaryMatrix(int[][] grid) {
+    if(grid == null || grid.length == 0 || grid[0].length == 0 || grid[0][0] == 1) return -1;
+
+    int rows = grid.length;
+    int cols = grid[0].length;
+
+    // initial setup
+    Queue<int[]> queue = new LinkedList<>();
+    boolean[][] visited = new boolean[grid.length][grid[0].length];
+    queue.offer(new int[] {0, 0});
+    visited[0][0] = true;
+
+    // BFS traversal
+    int ans = 0;
+    while(!queue.isEmpty()) {
+      ans++;
+      int size = queue.size();
+      while(size > 0) {
+        int[] curr = queue.poll();
+        if(curr[0] == rows-1 && curr[1] == cols - 1) return ans;
+
+        for(int[] direction: directions) {
+          int newX = curr[0] + direction[0];
+          int newY = curr[1] + direction[1];
+          if(newX >= 0 && newY >= 0 && newX < rows && newY < cols && grid[newX][newY] == 0 && !visited[newX][newY]) {
+            visited[newX][newY] = true;
+            queue.offer(new int[] {newX, newY});
+          }
+
+        }
+        size--;
+      }
+    }
+    return -1;
+  }
+
+
+}
