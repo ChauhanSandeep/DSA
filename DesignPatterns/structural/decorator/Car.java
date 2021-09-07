5a6e86e663b12e007cdca0fe717425f79f3f9804
SandeepChauhan <sschauhan@tekion.com>
2021-09-07 17:35:57 +0530

    Decorator pattern

---
+++ DesignPatterns/structural/decorator/Car.java
@@ -0,0 +1,70 @@
+package DesignPatterns.structural.decorator;
+
+public interface Car {
+
+	public void assemble();
+}
+
+/**
+ * BASIC CAR
+ */
+class BasicCar implements Car {
+
+	@Override
+	public void assemble() {
+		System.out.print("Assembling Basic Car.");
+	}
+
+}
+
+/**
+ * CAR DECORATOR
+ */
+class CarDecorator implements Car {
+
+	protected Car car;
+
+	public CarDecorator(Car c){
+		this.car=c;
+	}
+
+	@Override
+	public void assemble() {
+		this.car.assemble();
+	}
+
+}
+
+/**
+ * SPORTS CAR
+ */
+class SportsCar extends CarDecorator {
+
+	public SportsCar(Car c) {
+		super(c);
+		System.out.print("sports car features. ");
+	}
+
+	@Override
+	public void assemble(){
+		super.assemble();
+		System.out.print(" assembling features of Sports Car. ");
+	}
+}
+
+/**
+ * LUXURY CAR
+ */
+class LuxuryCar extends CarDecorator {
+
+	public LuxuryCar(Car c) {
+		super(c);
+		System.out.print("luxury car features. ");
+	}
+
+	@Override
+	public void assemble(){
+		super.assemble();
+		System.out.print(" Adding features of Luxury Car. ");
+	}
+}
---
+++ DesignPatterns/structural/decorator/Driver.java
@@ -0,0 +1,19 @@
+package DesignPatterns.structural.decorator;
+
+public class Driver {
+
+    public static void main(String[] args) {
+        System.out.println("Creating sports car");
+        Car sportsCar = new SportsCar(new BasicCar());
+        sportsCar.assemble();
+
+        System.out.println("\nCreating luxury car");
+        Car luxuryCar = new LuxuryCar(new BasicCar());
+        luxuryCar.assemble();
+
+        System.out.println("\ncreating luxury sports car");
+        Car luxurySportsCar = new SportsCar(new LuxuryCar(new BasicCar()));
+        luxurySportsCar.assemble();
+    }
+
+}
