c1bec010849c1b01a3d5e740fa00b2b87e6d285b
SandeepChauhan <sschauhan@tekion.com>
2021-09-14 09:20:41 +0530

    longest increasing subsequence

--- DynamicProgramming/LongestIncreasingSubsequence.java
+++ DynamicProgramming/LongestIncreasingSubsequence.java
@@ -1,5 +1,6 @@
 package DynamicProgramming;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class LongestIncreasingSubsequence {
@@ -11,22 +12,25 @@ public class LongestIncreasingSubsequence {
 
     /**
      * Find the length of longest increasing subsequence in array
+     *
      * @param arr
      * @return
      */
     public static int findLIS(int[] arr) {
-        if(arr == null || arr.length == 1) return 1;
-        int[] lisArr = new int[arr.length];
-        Arrays.fill(lisArr, 1);
+        if (arr == null || arr.length == 1) return 1;
+        int[] dp = new int[arr.length];
+        Arrays.fill(dp, 1); // either initiate all element as 1 or return result+1 in the end
 
+        int result = 1;
         for(int i=1; i<arr.length; i++) {
             for(int j=0; j<i; j++) {
-                if(arr[j] < arr[i] && lisArr[j] + 1 > lisArr[i]) {
-                    lisArr[i] = lisArr[j] + 1;
+                if(arr[j] < arr[i] && dp[j] + 1 > dp[i]) {
+                    dp[i] = dp[j] + 1;
+                    result = Math.max(result, dp[i]);
                 }
             }
 
         }
-        return lisArr[arr.length - 1];
+        return result;
     }
 }
