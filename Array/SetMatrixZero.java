c6a63edbd48e92547104c4753d713d655c337f44
SandeepChauhan <sschauhan@tekion.com>
2021-09-15 10:26:50 +0530

    set matrix to zero

---
+++ Array/SetMatrixZero.java
@@ -0,0 +1,51 @@
+package Array;
+
+public class SetMatrixZero {
+    public static void main(String[] args) {
+
+    }
+
+    public void setZeroes(int[][] matrix) {
+        boolean isCol = false;
+        int rowLen = matrix.length;
+        int colLen = matrix[0].length;
+
+        for (int i = 0; i < rowLen; i++) {
+            if (matrix[i][0] == 0) {
+                isCol = true;
+            }
+
+            for (int j = 1; j < colLen; j++) {
+                // If an element is zero, we set the first element of the corresponding row and column to 0
+                if (matrix[i][j] == 0) {
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+                }
+            }
+        }
+
+        // Iterate over the array once again and using the first row and first column, update the elements.
+        for (int i = 1; i < rowLen; i++) {
+            for (int j = 1; j < colLen; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        // See if the first row needs to be set to zero as well
+        if (matrix[0][0] == 0) {
+            for (int j = 0; j < colLen; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        // See if the first column needs to be set to zero as well
+        if (isCol) {
+            for (int i = 0; i < rowLen; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+
+}
