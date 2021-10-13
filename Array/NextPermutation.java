96384d4ad611c4bf4645ded2d78cd2f6b9e50964
SandeepChauhan <sschauhan@tekion.com>
2021-10-13 20:37:40 +0530

    add comments

--- Array/NextPermutation.java
+++ Array/NextPermutation.java
@@ -2,24 +2,38 @@ package Array;
 
 import java.util.Arrays;
 
+/**
+ * Implement next permutation, which rearranges numbers into the lexicographically next greater permutation of numbers.
+ * If such an arrangement is not possible, it must rearrange it as the lowest possible order (i.e., sorted in ascending order).
+ */
 public class NextPermutation {
     public static void main(String[] args) {
-        int[] nums = {4,2,0,2,3,2,0};
+        int[] nums = {1,5,8,4,7,6,5,3,1};
+        // 1,5,8,5,7,6,4,3,1
         nextPermutation(nums);
         System.out.println(Arrays.toString(nums));
 
     }
 
     /**
-     * Implement next permutation, which rearranges numbers into the lexicographically next greater permutation of numbers.
-     * If such an arrangement is not possible, it must rearrange it as the lowest possible order (i.e., sorted in ascending order).
+     * 1. find the index (pivot) where next element is greater the current element
+     * 2. find the smallest element on the right side of pivot which is greater than pivot element, and swap these two
+     * 3. reverse the right side of the pivot
+     *
+     * 1,5,8,4,7,6,5,3,1
+     * 1,5,8,5,7,6,4,3,1
+     * 1,5,8,5,1,3,4,6,7
+     *
+     * Note: go through each of these steps one by on in interview
      * @param nums
      */
     public static void nextPermutation(int[] nums) {
         int i = nums.length - 2;
+        // find the index (pivot) where next element is greater the current element
         while (i >= 0 && nums[i + 1] <= nums[i]) {
             i--;
         }
+        // find the smallest element on the right side of pivot which is greater than pivot element
         if (i >= 0) {
             int j = nums.length - 1;
             while (j >= 0 && nums[j] <= nums[i]) {
@@ -27,6 +41,7 @@ public class NextPermutation {
             }
             swap(nums, i, j);
         }
+        // reverse the right side of the pivot
         reverse(nums, i + 1);
     }
 
