1af43414eae8af08fcd4b70933a6e6ae5e202c21
SandeepChauhan <sschauhan@tekion.com>
2021-11-02 13:20:30 +0530

    longest happy string

--- String/LongestHappyString.java
+++ String/LongestHappyString.java
@@ -46,6 +46,43 @@ public class LongestHappyString {
         }
         return builder.toString();
     }
+
+    /**
+     * this is better approach with O(n) time complexity and O(1) space complexity
+     */
+    public String longestDiverseString2(int a, int b, int c) {
+        StringBuilder builder = new StringBuilder();
+
+        int total = a + b + c;
+        int currA = 0;
+        int currB = 0;
+        int currC = 0;
+
+        while(total > 0) {
+            if((a >= b && a >= c && currA < 2) || (a > 0 && (currB == 2 || currC == 2))) {
+                builder.append("a");
+                a--;
+                currA++;
+                currB = 0;
+                currC = 0;
+            } else if((b >= a && b >= c && currB < 2) || (b > 0 && (currA == 2 || currC == 2))) {
+                builder.append("b");
+                b--;
+                currB++;
+                currA = 0;
+                currC = 0;
+            }else if((c >= a && c >= b && currC < 2) || (c > 0 && (currA == 2 || currB == 2))) {
+                builder.append("c");
+                c--;
+                currC++;
+                currA = 0;
+                currB = 0;
+            }
+
+            total--;
+        }
+        return builder.toString();
+    }
 }
 
 class Pair {
