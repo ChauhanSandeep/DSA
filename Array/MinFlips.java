063c8587a8e34e87de4dc85b9808eafecccf3c04
SandeepChauhan <sschauhan@tekion.com>
2021-11-02 11:23:28 +0530

    find min flips required to change string to monotonic increasing

--- Array/MinFlips.java
+++ Array/MinFlips.java
@@ -3,15 +3,23 @@ package Array;
 /**
  * A binary string is monotone increasing if it consists of some number of 0's (possibly none), followed by some number of 1's (also possibly none).
  * You are given a binary string s. You can flip s[i] changing it from 0 to 1 or from 1 to 0.
+ *
+ * https://leetcode.com/problems/flip-string-to-monotone-increasing/
  */
 public class MinFlips {
 
     public static void main(String[] args) {
-        String input = "10011111110010111011";
+        String input = "00110000111111";
         int flips = new MinFlips().minFlipsMonoIncr(input);
         System.out.println(flips);
     }
 
+    /**
+     * Going by DP approach. when a character `ch` comes at the end of string, these are the cases:
+     * -> When '1' comes, no more flip should be applied, since '1' is appended to the tail of the original string.
+     * -> When '0' comes, things become a little bit complicated. There are two options for us: flip the newly appended '0' to '1', after flipCount flips for the original string; or flip oneCount '1' in the original string to '0'.
+     * Hence, the result of the next step of DP, in the '0' case, is std::min(flipCount + 1, oneCount);
+     */
     public int minFlipsMonoIncr(String str) {
         if(str == null || str.length() == 0) return 0;
 
@@ -21,13 +29,12 @@ public class MinFlips {
 
         for(int i=0; i<strArr.length; i++) {
             char curr = strArr[i];
-            if(curr == '0') {
-                if(oneCount == 0) continue;
-                flipCount++;
-            }else{
+            if(curr == '1') {
                 oneCount++;
+            }else{
+                flipCount++;
             }
-            if(flipCount > oneCount) flipCount = oneCount; // make more sense to flip ones.
+            flipCount = Math.min(oneCount, flipCount);
         }
         return flipCount;
     }
