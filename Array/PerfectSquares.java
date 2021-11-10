770c860d05352887a840304a8e74d9599808b838
SandeepChauhan <sschauhan@tekion.com>
2021-11-10 12:31:36 +0530

    perfect square count

--- Array/PerfectSquares.java
+++ Array/PerfectSquares.java
@@ -1,6 +1,7 @@
 package Array;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -12,8 +13,26 @@ import java.util.List;
  */
 public class PerfectSquares {
     public static void main(String[] args) {
-        int result = new PerfectSquares().numSquares(13);
+        int result = new PerfectSquares().numSquaresDp(13);
         System.out.println(result);
+    }
+
+    public int numSquaresDp(int n) {
+        final int[] dp = new int[n + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+
+        for (int i = 1; i * i <= n; i++) {
+            dp[i * i] = 1;
+        }
+
+        dp[0] = 0;
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j * j <= i; j++) {
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+            }
+        }
+
+        return dp[n];
     }
 
     public int numSquares(int n) {
