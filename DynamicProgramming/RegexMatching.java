8795b1c0bfbb76dced356a1b72965da44e0eb8f5
SandeepChauhan <sschauhan@tekion.com>
2021-12-19 13:48:57 +0530

    minor refactor

--- DynamicProgramming/RegexMatching.java
+++ DynamicProgramming/RegexMatching.java
@@ -11,13 +11,13 @@ import java.util.Arrays;
  */
 public class RegexMatching {
     public static void main(String[] args) {
-        System.out.println(new RegexMatching().isMatch("mississippi", "mis*is*p*."));
-        System.out.println(new RegexMatching().isMatch("mississippi", "mis*is*ip*."));
+        System.out.println(new RegexMatching().isMatch("mississippi", "mis*is*p*.")); // false
+        System.out.println(new RegexMatching().isMatch("mississippi", "mis*is*ip*.")); // true
     }
     /*
     https://youtu.be/DJvw8jCmxUU?t=1419
-    [          m      i      s       s      i      s       s     i     p        p     i
-      [true, false, false,  false, false, false, false, false, false, false, false, false],
+    [           m      i      s      *      i       s      *      i      p      *      .
+      [true,  false, false, false, false, false, false, false, false, false, false, false],
     m [false, true,  false, false, false, false, false, false, false, false, false, false],
     i [false, false, true,  false, true,  false, false, false, false, false, false, false],
     s [false, false, false, true,  true,  false, false, false, false, false, false, false],
@@ -28,8 +28,7 @@ public class RegexMatching {
     i [false, false, false, false, false, false, false, false, true,  false, true,  false],
     p [false, false, false, false, false, false, false, false, false, true,  true,  true],
     p [false, false, false, false, false, false, false, false, false, false, true,  true],
-    i [false, false, false, false, false, false, false, false, false, false, false, true]
-    ]
+    i [false, false, false, false, false, false, false, false, false, false, false, true]]
     */
     public boolean isMatch(String str, String pattern) {
         int strLen = str.length();
@@ -45,20 +44,22 @@ public class RegexMatching {
 
         for (int i = 1; i <= strLen; ++i) {
             for (int j = 1; j <= patternLen; ++j) {
-                char pChar = pattern.charAt(j - 1);
-                char sChar = str.charAt(i - 1);
-                if (pChar == '*') {
+                char currPatt = pattern.charAt(j - 1);
+                char currChar = str.charAt(i - 1);
+                if (currPatt == '*') {
                     dp[i][j] = dp[i][j - 2]; // take last to last char and ignore * and last character
-                    if (pattern.charAt(j - 2) == sChar || pattern.charAt(j - 2) == '.') {
-                        dp[i][j] = dp[i][j] || dp[i - 1][j];
+                    char prevPatt = pattern.charAt(j-2);
+                    if (prevPatt == currChar || prevPatt == '.') {
+                        dp[i][j] = dp[i][j] || dp[i - 1][j]; // extend from last matching
                     }
-                } else if (pChar == sChar || pChar == '.') {
+                } else if (currPatt == currChar || currPatt == '.') {
                     dp[i][j] = dp[i - 1][j - 1];
                 }else{
                     dp[i][j] = false;
                 }
             }
         }
+        System.out.println(Arrays.deepToString(dp));
         return dp[strLen][patternLen];
     }
 }
