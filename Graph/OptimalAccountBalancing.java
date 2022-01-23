e64f8523c4abf9652c9650e8f48c408c168935b5
SandeepChauhan <sschauhan@tekion.com>
2022-01-23 19:44:23 +0530

    optimal account balancing

---
+++ Graph/OptimalAccountBalancing.java
@@ -0,0 +1,63 @@
+package Graph;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * https://leetcode.com/problems/optimal-account-balancing/
+ * Splitwise
+ */
+public class OptimalAccountBalancing {
+
+    public static void main(String[] args) {
+        int[][] transactions = {{0, 1, 10}, {1, 0, 1}, {1, 2, 5}, {2, 0, 5}};
+        int minTransfers = new OptimalAccountBalancing().minTransfers(transactions);
+        System.out.println(minTransfers);
+    }
+
+    public int minTransfers(int[][] transactions) {
+        int[] debt = buildDebtArray(transactions); // Debt amount to balance for each person.
+
+
+        return getMinTransfersAfter(0, debt);
+    }
+
+    private int getMinTransfersAfter(int currId, int[] debt) {
+        while (currId < debt.length && debt[currId] == 0) {
+            currId++;
+        }
+
+        if (currId == debt.length) return 0;
+
+        int minTransactions = Integer.MAX_VALUE;
+        for (int i = currId + 1; i < debt.length; i++) {
+            if (debt[i] * debt[currId] < 0) {
+                debt[i] += debt[currId];
+                minTransactions = Math.min(minTransactions, getMinTransfersAfter(currId + 1, debt) + 1);
+                debt[i] -= debt[currId];
+            }
+        }
+
+        return minTransactions;
+    }
+
+    private int[] buildDebtArray(int[][] transactions) {
+        Map<Integer, Integer> debtMap = new HashMap<>(); // Map person ID to debt amount.
+
+        for (int[] transaction : transactions) {
+            int giver = transaction[0];
+            int taker = transaction[1];
+            int amount = transaction[2];
+            debtMap.put(giver, debtMap.getOrDefault(giver, 0) + amount);
+            debtMap.put(taker, debtMap.getOrDefault(taker, 0) - amount);
+        }
+
+        int[] debt = new int[debtMap.size()];
+        int i = 0;
+        for (int amount : debtMap.values()) {
+            debt[i++] = amount;
+        }
+
+        return debt;
+    }
+}
