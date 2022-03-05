8c8eec958278d7a6daa0a94b34c8ce8a8ff9ee4c
SandeepChauhan <sschauhan@tekion.com>
2022-03-05 11:46:34 +0530

    min window subsequence

--- DynamicProgramming/KnapsackRelated/MinWindowSubsequence.java
+++ DynamicProgramming/KnapsackRelated/MinWindowSubsequence.java
@@ -8,44 +8,57 @@ import java.util.Arrays;
 public class MinWindowSubsequence {
 
     public static void main(String[] args) {
-        String s1 = "abcdebd";
+        String s1 = "abcdebdde";
         String s2 = "bde";
         System.out.println(new MinWindowSubsequence().minWindow(s1, s2));
     }
 
     public String minWindow(String source, String target) {
-        int targetLen = target.length();
+        if(source.length() < target.length()) return "";
+
         int sourceLen = source.length();
+        int targetLen = target.length();
+
+        /**
+         * in dp[i][j]
+         * target substring 0-i
+         * source substring dp[i][j] - j
+         * dp[i][j] contains start of the source substring
+         */
         int[][] dp = new int[targetLen + 1][sourceLen + 1];
-//      dp[i][j] stores index of source which contains target subsequence from i to j
-        for (int j = 0; j <= sourceLen; j++) {
-            dp[0][j] = j + 1;
+        for(int[] row: dp) {
+            Arrays.fill(row, -1);
         }
 
-        for (int i = 1; i <= targetLen; i++) {
-            for (int j = 1; j <= sourceLen; j++) {
+        for(int j=0; j<sourceLen+1; j++) {
+            // j+1 because it will be used by right-down diagonal
+            dp[0][j] = j+1;
+        }
+
+        for(int i=1; i<targetLen+1; i++) {
+            for(int j=1; j<sourceLen+1; j++) {
                 char tchar = target.charAt(i-1);
                 char schar = source.charAt(j-1);
-                if (tchar == schar) {
-                    dp[i][j] = dp[i - 1][j - 1];
-                } else {
-                    dp[i][j] = dp[i][j - 1];
-                }
+                if(schar == tchar) dp[i][j] = dp[i-1][j-1];
+                else dp[i][j] = dp[i][j-1];
             }
         }
-        System.out.println(Arrays.deepToString(dp));
 
-        int resStart = 0;
+        int resStart = -1;
         int resLen = sourceLen + 1;
-        for (int j = 1; j <= sourceLen; j++) {
-            int currStart = dp[targetLen][j];
-            if ( currStart != 0) {
-                if (j - currStart + 1 < resLen) {
-                    resStart = currStart - 1;
-                    resLen = j - currStart + 1;
+        for(int j=1; j<sourceLen+1; j++) {
+            if(dp[targetLen][j] != -1) {
+                int currStart = dp[targetLen][j] - 1;
+                int currEnd = j-1;
+                int currLen = currEnd - currStart + 1;
+                if(currLen < resLen) {
+                    resLen = currLen;
+                    resStart = currStart;
                 }
             }
         }
-        return resLen == sourceLen + 1 ? "" : source.substring(resStart, resStart + resLen);
+        if(resLen == sourceLen + 1) return "";
+        return source.substring(resStart, resStart + resLen);
+
     }
 }
