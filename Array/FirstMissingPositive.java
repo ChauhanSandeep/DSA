18ed618efc6fa38e2342b2b440fc5b06f1f450a9
SandeepChauhan <sschauhan@tekion.com>
2022-03-08 12:10:54 +0530

    first missing positive number

--- Array/FirstMissingPositive.java
+++ Array/FirstMissingPositive.java
@@ -3,6 +3,9 @@ package Array;
 import java.util.Arrays;
 
 /**
+ * Given an array of integers containing element from -2^31 to 2^31 -1.
+ * Find the first positive number which is missing in array.
+ *
  * https://leetcode.com/problems/first-missing-positive/
  */
 public class FirstMissingPositive {
@@ -14,6 +17,7 @@ public class FirstMissingPositive {
     }
 
     public int firstMissingPositive(int[] nums) {
+        // Step1. check if 1 exists in array
         boolean containsOne = false;
         for (int num : nums) {
             if (num == 1) {
@@ -23,24 +27,27 @@ public class FirstMissingPositive {
         }
         if(!containsOne) return 1;
 
+        //Step2. If 1 exists, then replace elements <= 0 or element > size with 1
+        // Missing element will always be in range 1 to size
         for(int i= 0; i< nums.length; i++) {
             if(nums[i] <= 0 || nums[i] > nums.length){
                 nums[i] = 1;
             }
         }
 
+        // Step3. Iterate over array and mark index of found number with - sign
         for(int i= 0; i<nums.length; i++) {
             int curr = Math.abs(nums[i]);
-
             if(curr == nums.length) {
                 nums[0] = -Math.abs(nums[0]);
             } else {
                 nums[curr] = - Math.abs(nums[curr]);
             }
-
         }
         System.out.println(Arrays.toString(nums));
 
+        // Step4. Check the first number with positive sign.
+        // (This index was never marked hence this element is missing)
         for(int i=1; i<nums.length; i++) {
             if(nums[i] > 0)
                 return i;
