b07492d98873d11e12fdcd87c08cb1a2938cfe7e
SandeepChauhan <sschauhan@tekion.com>
2022-03-08 11:56:32 +0530

    refactor

--- Array/DuplicateNumber.java
+++ Array/DuplicateNumber.java
@@ -3,6 +3,7 @@ package Array;
 /**
  * Find duplicate number in array containing elements from 1 - n(duplicate occures 1 or more times)
  * You must solve the problem without modifying the array nums and uses only constant extra space.
+ *
  * https://leetcode.com/problems/find-the-duplicate-number/
  */
 public class DuplicateNumber {
@@ -13,39 +14,24 @@ public class DuplicateNumber {
         System.out.println(result);
     }
 
-    /**
-     * If count of numbers less than or equal to a current number are more than current number, then there is duplicate present in lower range of numbers
-     * Else duplicate is present in higher range of numbers.
-     * Time complexity O(nLogn) -> log n for binary search, n for finding count/occurence
-     */
     public int findDuplicate(int[] nums) {
-        int low = 1;
-        int high = nums.length - 1;
+        // This problem is like finding starting of loop in cyclic linked list
+        int slow = nums[0];
+        int fast = nums[0];
 
-        while (low <= high) {
-            int curr = (low + high) / 2;
-            int count = 0;
-            int occurence = 0;
-            for (int num : nums) {
-                if(num == curr) {
-                    occurence++;
-                    if(occurence > 1) return num;
-                }
-                if (num <= curr) {
-                    count++;
-                    if(count > curr) {
-                        break;
-                    }
-                }
-            }
+        // find collision in loop of numbers
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }while(slow != fast);
 
-            if (count > curr) {
-                high = curr - 1;
-            } else {
-                low = curr + 1;
-            }
+        // find the starting of loop
+        fast = nums[0];
+        while(slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
         }
-        return -1;
+        return slow;
     }
 
 }
