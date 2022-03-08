5f5d20cf7c7feb088aceea912f07c88f0650c8b4
SandeepChauhan <sschauhan@tekion.com>
2022-03-09 00:39:16 +0530

    subset backtracking

--- Backtracking/Subset.java
+++ Backtracking/Subset.java
@@ -5,33 +5,35 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * Given an integer array nums of unique elements, return all possible subsets (the power set).
+ *
+ * https://leetcode.com/problems/subsets/
+ */
 public class Subset {
     public static void main(String[] args) {
         int[] nums = {1, 2, 3};
-        List<List<Integer>> subsets = subsets(nums);
+        List<List<Integer>> subsets = new Subset().subsets(nums);
         System.out.println(subsets);
     }
 
-    /**
-     * Given an integer array nums of unique elements, return all possible subsets (the power set).
-     */
-    public static List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> resultList = new ArrayList<>();
-        subsetsHelper(resultList, new ArrayList<>(), nums, 0);
-        return resultList;
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        if(nums == null || nums.length ==0) return result;
+
+        List<Integer> curr = new ArrayList<>();
+        subsetRec(nums, curr, result, 0);
+        return result;
+
     }
 
-    private static void subsetsHelper(List<List<Integer>> resultList , ArrayList<Integer> list, int [] nums, int start){
-        list = (ArrayList<Integer>) new HashSet<>(list).stream().sorted((a, b) -> b - a).collect(Collectors.toList());
-        resultList.add(new ArrayList<>(list));
+    public void subsetRec(int[] nums, List<Integer> curr, List<List<Integer>> result, int start) {
+        result.add(new ArrayList<>(curr));
 
-        for(int i = start; i < nums.length; i++){
-            // add element
-            list.add(nums[i]);
-            // Explore
-            subsetsHelper(resultList, list, nums, i + 1);
-            // remove
-            list.remove(list.size() - 1);
+        for(int i= start; i<nums.length; i++) {
+            curr.add(nums[i]);
+            subsetRec(nums, curr, result, i+1);
+            curr.remove(curr.size() - 1);
         }
     }
 }
