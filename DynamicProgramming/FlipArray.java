d1e21e6bcd34505fdd04fe1a4e3a9999baa417c5
SandeepChauhan <sschauhan@tekion.com>
2022-03-26 18:46:20 +0530

    min flips

--- DynamicProgramming/FlipArray.java
+++ DynamicProgramming/FlipArray.java
@@ -16,28 +16,32 @@ public class FlipArray {
         System.out.println(result);
     }
 
-    public int solve(final int[] arr) {
-        int sum = 0;
-        int size = arr.length;
-        for (int i = 0; i < size; i++) {
-            sum += arr[i];
-        }
-        int[] dp = new int[2 * sum + 1];
-        Arrays.fill(dp, Integer.MAX_VALUE);
+    public int solve(int[] arr) {
+        int sum=0;
+        for(Integer element: arr) sum += element;
 
-        dp[2 * sum] = 0;
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j <= 2 * sum; j++) {
-                if (dp[j] != Integer.MAX_VALUE) {
-                    dp[j - 2 * arr[i]] = Math.min(dp[j] + 1, dp[j - 2 * arr[i]]);
+        int[] dp= new int[sum+1];
+        Arrays.fill(dp,Integer.MAX_VALUE);
+        dp[sum]=0; // 0 flips required to keep sum same.
+
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<=sum;j++){
+                if(dp[j]!=Integer.MAX_VALUE){
+                    int diff = 2*arr[i]; // difference in sum if current element is flipped
+                    if(j - diff >= 0){
+                        dp[j - diff] = Math.min(1 + dp[j], dp[j - diff]);
+                    }
                 }
             }
         }
-        for (int i = sum; i < 2 * sum + 1; i++) {
-            if (dp[i] != Integer.MAX_VALUE) {
-                return dp[i];
+
+        int ans=0;
+        for(int i=0;i<dp.length;i++){
+            if(dp[i]!=Integer.MAX_VALUE){
+                ans=dp[i];
+                break;
             }
         }
-        return 0;
+        return ans;
     }
 }
